fb0ba925acd9a6e22b5b4e271c6b954cecf086fd
Er Shubham kumar ojha <116981721+Ershubhamkrojha@users.noreply.github.com>
2023-12-25 22:15:03 +0530

    Update index.js

--- Api/index.js
+++ Api/index.js
@@ -31,7 +31,7 @@ const connect = async () => {
 app.use(express.urlencoded({extended : true}));
 app.use(express.json());
 app.use(cors({
-    origin:'${process.env.path}'
+    origin:'http://localhost:3000'
 }));
 app.use(cookieParser());
 app.use(express.json())
